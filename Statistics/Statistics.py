7e930b98d1a31ca4368304f6c17d9083e98a8835
Kaan <kaan.eksen@std.yeditepe.edu.tr>
2019-08-26 07:39:38 +0300

    Statistics
    * Statistics is improved.

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -2,19 +2,51 @@ from Database.MongoDB import Mongo
 import socket
 from datetime import datetime
 from datetime import date
+import calendar
+import os
+from ConfigManager import Config
 
+def load_config():
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    Config.add_config_ini('%s/main.ini' % pwd)
 
 class Statistics(object):
+    START_YEAR=2016
+    END_YEAR=2018
+    categories = ['business', 'politics', 'energy', 'commodities', 'stocks', 'exclusive', 'economy', 'world', 'technology', 'money',
+     'news', 'economics', 'tech', 'bonds', ''];
 
     def collect(self):
         socket.setdefaulttimeout(120)  # 120 seconds
         db=Mongo()
 
-        start = datetime.datetime(1998, 1, 1, 1, 35, 6, 764)
-        end = datetime.datetime(1999, 1, 1, 1, 55, 3, 381)
-        print(start)
-        for doc in db.instance.db.find({'time': {'$gte': start, '$lt': end}}):
-            print(doc)
+        start = datetime(self.START_YEAR, 11, 1, 0, 0, 0, 0)
+        end = datetime(self.START_YEAR, 11+1, 1, 0, 0, 0, 0)
+        collection = db.instance.db.get_collection(Config.database.collection)
+        print("\t", end='\t')
+        for category in self.categories:
+            print(category, end='\t')
+        print()
+        while end.year < self.END_YEAR:
+            count = collection.find({'RSS_Date': {'$gte': start, '$lt': end}}).count(False)
+            # Get Category Count
+            result = collection.aggregate([{ '$match': { 'RSS_Date': {'$gte': start, '$lt': end},} },
+                                { "$group": { "_id": { "$toLower": "$RSS_Category" }, "count": { "$sum": 1 } } },
+                                { "$group": { "_id": None, "counts": { "$push": { "k": "$_id", "v": "$count" } } } },
+                                { "$replaceRoot": { "newRoot": { "$arrayToObject": "$counts" } } } ])
+            print(str(start.year) + "." + str(start.month) + " \t " + str(count), end='\t')
+            listResult = list(result)
+            for item in listResult:
+                for category in self.categories:
+                    if category in item:
+                        print(item[category], end='\t')
+                    else:
+                        print('0', end='\t')
+            print()
+            start = Statistics.add_one_month(start)
+            end = Statistics.add_one_month(end)
+        #for doc in db.instance.db.find({'time': {'$gte': start, '$lt': end}}):
+        #    print(doc)
 
     @staticmethod
     def add_years(d, years):
@@ -28,3 +60,17 @@ class Statistics(object):
             return d.replace(year = d.year + years)
         except ValueError:
             return d + (date(d.year + years, 1, 1) - date(d.year, 1, 1))
+
+    def add_one_month(orig_date):
+        # advance year and month by one month
+        new_year = orig_date.year
+        new_month = orig_date.month + 1
+        # note: in datetime.date, months go from 1 to 12
+        if new_month > 12:
+            new_year += 1
+            new_month -= 12
+
+        last_day_of_month = calendar.monthrange(new_year, new_month)[1]
+        new_day = min(orig_date.day, last_day_of_month)
+
+        return orig_date.replace(year=new_year, month=new_month, day=new_day)
