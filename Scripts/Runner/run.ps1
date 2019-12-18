4abf667401f97475ecb1f19d6c3c14aa4eca2d6b
Kaan <kaan.eksen@std.yeditepe.edu.tr>
2019-12-19 01:06:33 +0300

    Runner - Ex
    * Runner - Example is added.

--- Scripts/Runner/run.ps1
+++ Scripts/Runner/run.ps1
@@ -1,6 +1,6 @@
 # Settings
 [string]$CondaEnvirment = "MScThesis"
-[string]$Location = "C:\Users\eksen\Documents\Test\MScThesis"
+[string]$Location = "C:\Users\eksen\Documents\GitHub\MScThesis"
 [string]$WordEmbGoogle = "I:\Glove\GoogleNews-vectors-negative300.bin"
 [string]$WordEmbGoogleSize = 300
 [string]$WordEmbGlove = "I:\Glove\glove.6B.100d.w2v.txt"
@@ -25,10 +25,16 @@ Import-Module $PSScriptRoot\"ShoutdownModule.psm1";
 # Change Settings
 <#  Example
     Change-Network-Type -ConfigPath $ConfigPath -Value "News" -WikiMultiplyFactors 10 -TweetMultiplyFactors 10 -TweetColumn "tweet_percentage" #News ,Wiki, WikiAndTweet
-    Change-WordEmbedding-Settings -ConfigPath $ConfigPath -EmbNetPath $WordEmbGoogle -EmbNetSize $WordEmbGoogle
+    Change-WordEmbedding-Settings -ConfigPath $ConfigPath -EmbNetPath $WordEmbGoogle -EmbNetSize $WordEmbGoogleSize
     Change-Network-Settings -ConfigPath $ConfigPath -Epochs 100 -BatchSize 100 -SequenceLength 100 -Criterion "100" -Optimizer "100" -UseGPU $true
     Change-Database -ConfigPath $ConfigPath -DatabaseName "FilteredNewsGeneral" -Category $BussinesCategories
 #>
 
+#First Test
+Change-WordEmbedding-Settings -ConfigPath $ConfigPath -EmbNetPath $WordEmbGoogle -EmbNetSize $WordEmbGoogleSize
+Change-Network-Type -ConfigPath $ConfigPath -Value "WikiAndTweet" -WikiMultiplyFactors 30 -TweetMultiplyFactors 30 -TweetColumn "tweet_percentage" #News ,Wiki, WikiAndTweet
+Change-Network-Settings -ConfigPath $ConfigPath -Epochs 5 -BatchSize 20 -SequenceLength 200 -Criterion "NLLLoss" -Optimizer "Adam" -UseGPU $false
+Change-Database -ConfigPath $ConfigPath -DatabaseName "FilteredNewsGeneralNoTag" -Category $BussinesCategories
+Run-Python-Script -ConfigPath $ConfigPath -CondaEnv $CondaEnvirment
 
 ShutDown-With-Popup
