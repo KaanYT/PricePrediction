b1327cf61fd43b3cfa1a7dad41ea837dd736c544
Kaan <kaan.eksen@std.yeditepe.edu.tr>
2019-12-19 07:42:43 +0300

    Update run.ps1
    * Bug fix

--- Scripts/Runner/run.ps1
+++ Scripts/Runner/run.ps1
@@ -27,7 +27,7 @@ Import-Module $PSScriptRoot\"ShoutdownModule.psm1";
     Change-Network-Type -ConfigPath $ConfigPath -Value "News" -WikiMultiplyFactors 10 -TweetMultiplyFactors 10 -TweetColumn "tweet_percentage" #News ,Wiki, WikiAndTweet
     Change-WordEmbedding-Settings -ConfigPath $ConfigPath -EmbNetPath $WordEmbGoogle -EmbNetSize $WordEmbGoogleSize
     Change-Network-Settings -ConfigPath $ConfigPath -Epochs 100 -BatchSize 100 -SequenceLength 100 -Criterion "100" -Optimizer "100" -UseGPU $true
-    Change-Database -ConfigPath $ConfigPath -DatabaseName "FilteredNewsGeneral" -Category $BussinesCategories
+    Change-Database -ScriptPath $ConfigPath -DatabaseName "FilteredNewsGeneral" -Category $BussinesCategories
 #>
 
 #First Test
@@ -35,6 +35,5 @@ Change-WordEmbedding-Settings -ConfigPath $ConfigPath -EmbNetPath $WordEmbGoogle
 Change-Network-Type -ConfigPath $ConfigPath -Value "WikiAndTweet" -WikiMultiplyFactors 30 -TweetMultiplyFactors 30 -TweetColumn "tweet_percentage" #News ,Wiki, WikiAndTweet
 Change-Network-Settings -ConfigPath $ConfigPath -Epochs 5 -BatchSize 20 -SequenceLength 200 -Criterion "NLLLoss" -Optimizer "Adam" -UseGPU $false
 Change-Database -ConfigPath $ConfigPath -DatabaseName "FilteredNewsGeneralNoTag" -Category $BussinesCategories
-Run-Python-Script -ConfigPath $ConfigPath -CondaEnv $CondaEnvirment
+Run-Python-Script -ScriptPath $Location -CondaEnv $CondaEnvirment
 
-ShutDown-With-Popup
